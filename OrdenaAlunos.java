4a8beeaa10c21217c7fa04bcbac6f4490e1757dc
leodantas96 <leodantas96@gmail.com>
2017-01-24 10:17:02 -0300

    Create OrdenaAlunos.java

---
+++ OrdenaAlunos.java
@@ -0,0 +1,52 @@
+/**
+ *
+ * @author Leonardo Luiz do Nascimento Dantas Filho
+ *Ultima atualização: 24/01/2017
+ */
+public class OrdenaAlunos{
+        public static void main(String[] args){
+         ArrayList<Alunos> aluno = new ArrayList<Alunos>();
+
+         Parsing p = new Parsing(aluno,1000);//quantidade de instâncias a serem lidas
+         p.run();
+         /*
+         for(Alunos a : aluno){
+
+         System.out.println("Matricula:"+a.getMatricula()+"\nNome:"+a.getNome()+"\nIngresso:"+a.getNascimento() );
+
+     }
+         */
+
+         
+
+         
+         SelectionSort s = new SelectionSort();
+         InsertionSort i = new InsertionSort();
+        
+
+        
+         s.SSort(aluno);
+         i.insercao(aluno);
+        
+        
+        
+
+         //System.out.println(aluno.size());
+
+
+         for(Alunos a : aluno){
+
+         System.out.println("Matricula:"+a.getMatricula()+"\nNome:"+a.getNome()+"\nNascimento:"+a.getNascimento() );
+
+     }  
+
+    
+   }
+
+    
+   
+     
+   
+}
+
+

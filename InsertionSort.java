ad9cedc9035da460bdce1e3d3f5794a9ea6a54cf
leodantas96 <leodantas96@gmail.com>
2017-01-25 13:06:22 -0300

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,6 +1,4 @@
-package APA;
-
-import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  *
@@ -8,22 +6,25 @@ import java.util.ArrayList;
  */
 public class InsertionSort {
     
-    public void insercao(ArrayList<Alunos> aluno){
+    public static void insercao(Comparable [ ] a){
         int i,j,cont=0;
         long tempoInicial = System.currentTimeMillis();  
-        for(j=1;j<aluno.size();j++){
-            Alunos aux = aluno.get(j);
-            int x = aluno.get(j).getMatricula(); 
-             for(i=j-1; i>=0 &&  (aluno.get(i).getMatricula())> x; i--){
-             aluno.set(i+1, aluno.get(i));
+        for(j=1;j<a.length;j++){
+            Comparable aux = a[j];
+            Comparable x = a[j]; 
+             for(i=j-1; i>=0 && a[i].compareTo(x)>0; i--){
+             a[i+1] = a[i];
              cont++;
            }
            
-           aluno.set(i+1,aux);
+           a[i+1]=aux;
            
         }
         long tempoFinal = System.currentTimeMillis();  
         System.out.println("Trocas "+cont+" Tempo:"+(tempoFinal - tempoInicial) / 1000d);
-    }
     
-}
+    }
+
+  
+}  
+    

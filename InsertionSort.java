9899bb28eba76f383d88558cca144e81ded3a85b
leodantas96 <leodantas96@gmail.com>
2017-01-25 13:12:12 -0300

    Create InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+/**
+ *
+ * @author Dell
+ */
+public class InsertionSort {
+    
+    public static void insercao(Comparable [ ] a){
+        int i,j,cont=0;
+        long tempoInicial = System.currentTimeMillis();  
+        for(j=1;j<a.length;j++){
+            Comparable aux = a[j];
+            Comparable x = a[j]; 
+             for(i=j-1; i>=0 && a[i].compareTo(x)>0; i--){
+             a[i+1] = a[i];
+             cont++;
+           }
+           
+           a[i+1]=aux;
+           
+        }
+        long tempoFinal = System.currentTimeMillis();  
+        System.out.println("Trocas "+cont+" Tempo:"+(tempoFinal - tempoInicial) / 1000d);
+    
+    }
+
+  
+}  
+    
